2816b7022d7bd9aad2a8b873465cd6a00473efb1
Máirtín Ó Flatharta <mairtinflatherty2014@gmail.com>
2021-10-12 15:07:19 +0100

    Add method for printing list items

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args){
+        // First, create a bunch of test objects
         Student student1 = new Student(
                 "James",
                 (short)22,
@@ -67,6 +68,7 @@ public class Main {
                 DateTime.parse("2024-06-01")
         );
 
+        // Step 2: Create connections between test objects
         course1.addModule(module1);
         course1.addModule(module2);
         course1.addStudent(student1);
@@ -84,7 +86,94 @@ public class Main {
 
         module3.addStudent(student3);
 
-        System.out.println(course1.getName() + ":\n" + course1.getModules());
-        System.out.println(course2.getName() + ":\n" + course2.getModules());
+        // Step 3: Print the names of objects and the contents
+        // of their arrayLists
+        System.out.println("Courses:");
+        System.out.println(course1.getName() + " Students:");
+        printListNames(course1.getStudents());
+        System.out.println();
+        System.out.println(course1.getName() + " Modules:");
+        printListNames(course1.getModules());
+        System.out.println();
+
+        System.out.println(course2.getName() + " Students:");
+        printListNames(course2.getStudents());
+        System.out.println();
+        System.out.println(course2.getName() + " Modules:");
+        printListNames(course2.getModules());
+        System.out.println("===========================================");
+
+        System.out.println("Modules:");
+        System.out.println(module1.getName() + " Courses:");
+        printListNames(module1.getCourses());
+        System.out.println();
+        System.out.println(module1.getName() + " Students:");
+        printListNames(module1.getStudents());
+        System.out.println();
+
+        System.out.println(module2.getName() + " Courses:");
+        printListNames(module2.getCourses());
+        System.out.println();
+        System.out.println(module2.getName() + " Students:");
+        printListNames(module2.getStudents());
+        System.out.println();
+
+        System.out.println(module3.getName() + " Courses:");
+        printListNames(module3.getCourses());
+        System.out.println();
+        System.out.println(module3.getName() + " Students:");
+        printListNames(module3.getStudents());
+        System.out.println("===========================================");
+
+        System.out.println("Students:");
+        System.out.println(student1.getName() + " Courses:");
+        printListNames(student1.getCourses());
+        System.out.println();
+        System.out.println(student1.getName() + " Modules:");
+        printListNames(student1.getModules());
+        System.out.println();
+
+        System.out.println(student2.getName() + " Courses:");
+        printListNames(student2.getCourses());
+        System.out.println();
+        System.out.println(student2.getName() + " Modules:");
+        printListNames(student2.getModules());
+        System.out.println();
+
+        System.out.println(student3.getName() + " Courses:");
+        printListNames(student3.getCourses());
+        System.out.println();
+        System.out.println(student3.getName() + " Modules:");
+        printListNames(student3.getModules());
+        System.out.println("===========================================");
+    }
+
+    public static <T> void printListNames(ArrayList<T> list){
+        // Take in an ArrayList of abstract objects and print them
+        // This could be replaced by 3 different methods to print each
+        // type of list, but it works for now
+        for (Object o : list) {
+            try{
+                System.out.println(((Student)o).getName());
+                continue;
+            }
+            catch (ClassCastException e) {
+                // Not a student list. Continue
+            }
+            try{
+                System.out.println(((Module)o).getName());
+                continue;
+            }
+            catch (ClassCastException e) {
+                // Not a Module list. Continue
+            }
+            try{
+                System.out.println(((Course)o).getName());
+            }
+            catch (ClassCastException e) {
+                System.out.println("Some unknown class was entered. Exiting...");
+                return;
+            }
+        }
     }
 }
